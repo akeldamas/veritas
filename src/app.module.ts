e520811f22161e60d9b19c229145e39f521d8d91
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2024-07-21 12:15:47 +0200

    Add typeorm module and set db environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService, ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig, { CONFIG_DATABASE } from './config/database.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env.development', '.env.production'],
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const dbConfig =
+          configService.get<ConfigType<typeof databaseConfig>>(CONFIG_DATABASE);
+        return {
+          type: 'postgres',
+          host: dbConfig.host,
+          port: dbConfig.port,
+          username: dbConfig.username,
+          password: dbConfig.password,
+          database: dbConfig.database,
+          synchronize: dbConfig.synchronize,
+          autoLoadEntities: true,
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
